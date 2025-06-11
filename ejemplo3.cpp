5aa061a60a070966f56af9342c00c98ecd70e4e2
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-06-11 13:42:00 -0500

    Agregar ejemplo 3: AVANCE DEL EJERCICIO

---
+++ ejemplo3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,A[30];
+    cout<<"Ingrese el numero de elementos que desea ingresar: ";
+    cin>>n;
+    for (int i=1; i<=n; i++) {
+        cin>>A[i];
+    }
+    for (int i=1; i<=n; i++) {
+
+    }
+}
