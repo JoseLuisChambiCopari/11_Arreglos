6358f5f42ea825baa1e1baabb6c9ff484a3218c1
JoseLuisChambiCopari <jchambic@unjbg.edu.pe>
2025-06-11 19:40:27 -0500

    Actualiza ejemplo 3: Se completa el ejemplo sobre intercambiar elementos equidistantes

--- ejemplo3.cpp
+++ ejemplo3.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n,A[30];
+    int aux,n,A[30];
     cout<<"Ingrese el numero de elementos que desea ingresar: ";
     cin>>n;
     for (int i=1; i<=n; i++) {
-        cin>>A[i];
+        cout<<"Ingrese el numero: ";
+		cin>>A[i];
     }
-    for (int i=1; i<=n; i++) {
-
+    for (int i=1; i<=(n/2); i++) {
+        aux=A[i];
+        A[i]=A[n-i+1];
+        A[n-i+1]=aux;
     }
+    cout<<"El intercambio quedaria de la siguiente manera: "<<endl;
+    for (int i=1; i<=n;i++) {
+        cout<<A[i]<<";";
+    }
+    return 0;
 }
